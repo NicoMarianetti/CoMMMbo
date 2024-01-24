24863b3a802858cc5e32ae6336659a04e5200857
DanielMarianetti <danimarianetti@gmail.com>
2024-01-24 10:34:29 -0300

    init
    Fix superposition with following component

--- src/sections/05-FeatureProjects/FeatureProjects.tsx
+++ src/sections/05-FeatureProjects/FeatureProjects.tsx
@@ -2,5 +2,45 @@ import React from 'react';
 import './FeatureProjects.css';
 
 export const FeatureProjects = () => {
-  return <div>FeatureProjects</div>;
+  const posters: string[] = [
+    'BaiBaiProject.png',
+    'VirtualDotProject.png'
+  ];
+
+  const settings = {
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    focusOnSelect: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    // <div>TEST</div>S
+  <div className='featuredProjects font-roboto'>
+    <div className='featuredProjectsTitle'>
+      FEATURED PROJECTS
+    </div>
+    <div className='logoContainer'>
+      {/* Ver por que se ve 1 y medio */}
+      {posters.map((poster, index) => (
+          <img
+            key={index}
+            src={require(`../../assets/img/${poster}`)}
+            alt={poster}
+            className='logo'
+          />
+        ))}
+    </div>
+    
+  </div>
+  );
 };
