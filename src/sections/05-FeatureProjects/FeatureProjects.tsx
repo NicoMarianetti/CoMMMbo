47bf2add77100c3e7ec131ac49e5889a6400b1fd
NicoMarianetti <nmarianetti@fi.uba.ar>
2024-02-20 19:49:35 -0300

    Finishing Feature projects and fixing responsiveness

--- src/sections/05-FeatureProjects/FeatureProjects.tsx
+++ src/sections/05-FeatureProjects/FeatureProjects.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './FeatureProjects.css';
-import {Typography} from '../../assets/typography';
+import {Typography, TypographyHeader} from '../../assets/typography';
 import {theme} from '../../assets/theme';
 import Slider from 'react-slick';
 
+type Poster = {
+  title: string;
+  tags: string[];
+  img: string;
+  url: string;
+};
+
 export const FeatureProjects = () => {
-  const posters: string[] = ['BaiBaiProject.png', 'VirtualDotProject.png'];
+  const posters: Poster[] = [
+    {
+      img: 'VirtualDotProject.png',
+      title: 'VIRTUAL DOT',
+      tags: ['Branding', 'Web', 'Newsletter'],
+      url: '',
+    },
+    {
+      img: 'BaiBaiProject.png',
+      title: 'BAI BAI',
+      tags: ['Social Media', 'Branding'],
+      url: '',
+    },
+  ];
 
   const proyectsTitleStyles = {
     fontSize: '60px',
@@ -14,36 +34,89 @@ export const FeatureProjects = () => {
     marginBottom: '0px',
   };
 
+  const titleStyles = {
+    fontSize: '100px',
+    fontWeight: '700',
+    color: theme.text.main,
+    margin: 0,
+    lineHeight: '1',
+  };
+
+  const tagStyles = {
+    fontSize: '30px',
+    color: theme.text.main,
+    letterSpacing: '2px',
+    margin: 0,
+  };
+
+  const numberPostStyle = {
+    fontSize: '80px',
+    fontWeight: '100',
+    color: theme.text.main,
+    margin: 0,
+    letterSpacing: '2px',
+  };
+
   const settings = {
-    infinite: false,
-    speed: 300,
+    // infinite: true
+    speed: 3000,
+    // autoplay: true,
     arrows: false,
     focusOnSelect: false,
-    autoplay: true,
-    centerMode: true,
-    slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+    slidesToShow: 1.9,
   };
 
   return (
     <div className="featuredProjects">
-      <Typography style={proyectsTitleStyles}>FEATURED PROJECTS</Typography>
+      <div className="titleProjects">
+        <Typography style={proyectsTitleStyles}>FEATURED PROJECTS</Typography>
+      </div>
       <Slider className="logoSlider" {...settings}>
         {posters.map((poster, index) => (
-          <div className="logoContainer">
+          <div className="logoContainerProjects">
             <img
               key={index}
-              src={require(`../../assets/img/${poster}`)}
-              alt={poster}
+              src={require(`../../assets/img/${poster.img}`)}
+              alt={poster.title}
               className="logo"
             />
+
+            <div className="infoProject">
+              <TypographyHeader style={titleStyles}>
+                {poster.title}
+              </TypographyHeader>
+
+              <div className="tagsProjects">
+                {poster.tags.map((tag, index) => {
+                  const isLastTag = index === poster.tags.length - 1;
+
+                  if (!isLastTag) {
+                    return (
+                      <div className="pointTag">
+                        <TypographyHeader style={tagStyles} key={index}>
+                          {tag}
+                        </TypographyHeader>
+                        <TypographyHeader
+                          style={tagStyles}
+                          key={'point' + index}>
+                          .
+                        </TypographyHeader>
+                      </div>
+                    );
+                  }
+
+                  return (
+                    <TypographyHeader style={tagStyles}>{tag}</TypographyHeader>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="numberProject">
+              <Typography style={numberPostStyle}>{`00${
+                index + 1
+              }_`}</Typography>
+            </div>
           </div>
         ))}
       </Slider>
